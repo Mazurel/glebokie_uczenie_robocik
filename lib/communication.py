5c943147ed22c3aa56db64931399e3ba7da9963b
Mateusz Mazur <mateusz.mazur@e.email>
2024-02-01 19:17:37 +0100

    Make robot movement discrete
    Signed-off-by: Mateusz Mazur <mateusz.mazur@e.email>

---
+++ lib/communication.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+
+import requests
+from requests.adapters import HTTPAdapter
+
+ROBOT_IP = "http://192.168.219.157"
+
+MAX_SPEED = 250
+MIN_SPEED = -250
+REGULAR_SPEED_TURN = 250
+REGULAR_SPEED = 180
+REGULAR_SPEED_FORWARD = 140
+
+MAX_RIGHT = 27
+TURN_CHANGE = 10
+MAX_LEFT = -13
+STRAIGHT = 7
+
+turn = STRAIGHT
+speed = 0
+prev_speed = 0
+prev_turn = STRAIGHT
+finished = False
+
+session = requests.Session()
+session.mount(ROBOT_IP, HTTPAdapter(max_retries=1))
+
+
+def download_and_save_photo(file: Path):
+    global speed, turn, folder_path
+    url = f"{ROBOT_IP}/photo"
+
+    if file.suffix != ".png":
+        raise ValueError("Invalid file path (should have `.png` extension)")
+
+    try:
+        response = session.get(url)
+        if response.status_code == 200:
+            with open(file.as_posix(), 'wb') as img_file:
+                img_file.write(response.content)
+            print(f"Saved photo to {file.as_posix()}")
+        else:
+            print(f"Failed to download photo. Status code: {response.status_code}")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+# Przeskalowanie wartości do obsługi pada
+def rescale(value, in_min, in_max, out_min, out_max):
+    return int((value - in_min) / (in_max - in_min) * (out_max - out_min) + out_min)
+
+
+def update_speed(value, reverse=False):
+    global speed
+    value = (value + 1.0) / 2.0
+    if reverse:
+        value = -rescale(value, 0, 1, 0, MAX_SPEED)
+    else:
+        value = rescale(value, 0, 1, 0, MAX_SPEED)
+
+    speed = max(MIN_SPEED, min(MAX_SPEED, value))
+
+
+def update_turn(value):
+    global turn
+    value = rescale(value, -1, 1, MAX_LEFT, MAX_RIGHT)
+
+    turn = max(MAX_LEFT, min(MAX_RIGHT, value))
+
+
+def send_command() -> bool:
+    """Returns true when something was sent."""
+    global prev_speed, prev_turn
+    if speed != prev_speed or turn != prev_turn:
+        print("Sending command ...")
+        url = f"{ROBOT_IP}/drive?speed={speed}&turn={turn}"
+        session.get(url, timeout=200)
+        prev_speed, prev_turn = speed, turn
+        return True
+    return False
+
+def reset(full: bool = False):
+    global turn, speed
+    speed = 0
+    if full:
+        turn = (MAX_LEFT + MAX_RIGHT) // 2
--- main.py
+++ main.py
@@ -1,208 +1,101 @@
-import os
-import requests
-import keyboard
-import pygame
 import sys
 import time
+from pathlib import Path
+import json
 
-ROBOT_IP = "http://192.168.70.157"
+import keyboard
 
-MAX_SPEED = 250
-MIN_SPEED = -250
-REGULAR_SPEED_TURN = 250
-REGULAR_SPEED = 180
-REGULAR_SPEED_FORWARD = 140
+from lib import communication
 
-MAX_RIGHT = 27
-TURN_CHANGE = 10
-MAX_LEFT = -13
-STRAIGHT = 7
-turn = STRAIGHT
-speed = 0
-prev_speed = 0
-prev_turn = STRAIGHT
-is_recording = False
-i = 1
+class Keyboard:
+    def __init__(self) -> None:
+        self.handlers = []
 
-folder_path = r"C:\records"
-save_path = r"C:\records"
+    def clear_handlers(self):
+        for handler in self.handlers:
+            keyboard.remove_hotkey(handler)
+        self.handlers = []
 
+    def enable(self):
+        self.clear_handlers()
+        self.handlers = [
+            keyboard.add_hotkey("up", forward),
+            keyboard.add_hotkey("w", forward),
+            keyboard.add_hotkey("down", backward),
+            keyboard.add_hotkey("s", backward),
+            keyboard.add_hotkey("left", left),
+            keyboard.add_hotkey("a", left),
+            keyboard.add_hotkey("right", right),
+            keyboard.add_hotkey("d", right),
+            keyboard.add_hotkey("q", quit),
+        ]
 
-def create_new_folder():
-    global i, save_path
-    i = 1
-    # Create a new folder with a timestamp as its name
-    timestamp = time.strftime("%Y%m%d%H%M%S")
-    folder_name = f"Record_{timestamp}"
-    new_folder_path = os.path.join(save_path, folder_name)
+def forward():
+    communication.speed = communication.REGULAR_SPEED_FORWARD
 
-    try:
-        os.makedirs(new_folder_path)
-        return new_folder_path
-    except OSError as e:
-        print(f"Error creating folder: {e}")
-        return None
+def backward():
+    communication.speed = -communication.REGULAR_SPEED_FORWARD
 
+def left():
+    communication.turn = max(communication.turn - communication.TURN_CHANGE, communication.MAX_LEFT)
 
-def download_and_save_photo():
-    global i, speed, turn, folder_path
-    url = f"{ROBOT_IP}/photo"
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            img_path = os.path.join(folder_path, f"Record_{i}_speed={speed}&turn={turn}.png")
-            i += 1
-            with open(img_path, 'wb') as img_file:
-                img_file.write(response.content)
-        else:
-            print(f"Failed to download photo. Status code: {response.status_code}")
+def right():
+    communication.turn = min(communication.turn + communication.TURN_CHANGE, communication.MAX_RIGHT)
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-# Przeskalowanie wartości do obsługi pada
-def rescale(value, in_min, in_max, out_min, out_max):
-    return int((value - in_min) / (in_max - in_min) * (out_max - out_min) + out_min)
-
-
-def update_speed(value, reverse=False):
-    global speed
-    value = (value + 1.0) / 2.0
-    if reverse:
-        value = -rescale(value, 0, 1, 0, MAX_SPEED)
-    else:
-        value = rescale(value, 0, 1, 0, MAX_SPEED)
-
-    speed = max(MIN_SPEED, min(MAX_SPEED, value))
-
-
-def update_turn(value):
-    global turn
-    value = rescale(value, -1, 1, MAX_LEFT, MAX_RIGHT)
-
-    turn = max(MAX_LEFT, min(MAX_RIGHT, value))
-
-
-def send_command():
-    global prev_speed, prev_turn
-    if speed != prev_speed or turn != prev_turn:
-        url = f"{ROBOT_IP}/drive?speed={speed}&turn={turn}/photo"
-        requests.get(url, stream=True)
-        prev_speed, prev_turn = speed, turn
-
-
-def handle_keyboard():
-    global speed, turn, is_recording, save_path, folder_path
-
-    if keyboard.is_pressed("up") or keyboard.is_pressed("w"):
-        if turn == STRAIGHT:
-            speed = REGULAR_SPEED_FORWARD
-        elif turn == MAX_RIGHT or turn == MAX_LEFT:
-            speed = REGULAR_SPEED_TURN
-        else:
-            speed = REGULAR_SPEED
-    elif keyboard.is_pressed("down") or keyboard.is_pressed("s"):
-        if turn == STRAIGHT:
-            speed = -REGULAR_SPEED_FORWARD
-        elif turn == MAX_RIGHT or turn == MAX_LEFT:
-            speed = -REGULAR_SPEED_TURN
-        else:
-            speed = -REGULAR_SPEED
-    elif keyboard.is_pressed("t"):
-        speed = MAX_SPEED
-    else:
-        speed = 0
-
-    if keyboard.is_pressed("left") or keyboard.is_pressed("a"):
-        turn = max(turn - TURN_CHANGE, MAX_LEFT)
-    elif keyboard.is_pressed("right") or keyboard.is_pressed("d"):
-        turn = min(turn + TURN_CHANGE, MAX_RIGHT)
-
-    if keyboard.is_pressed("r"):
-        is_recording = True
-        print("Record started")
-        folder_path = create_new_folder()
-    elif keyboard.is_pressed("e"):
-        is_recording = False
-        print("Record stopped")
-
-
-def handle_controller():
-    global speed, turn, is_recording, save_path, folder_path
-    for event in pygame.event.get():
-        if event.type == pygame.JOYAXISMOTION:
-            if event.axis == 5:  # R2 for acceleration
-                update_speed(event.value, reverse=False)
-            elif event.axis == 4:  # L2 for backward
-                update_speed(event.value, reverse=True)
-            elif event.axis == 0:  # Left stick horizontal axis for turn
-                update_turn(event.value)
-
-        elif event.type == pygame.JOYBUTTONDOWN:
-            if event.button == 1:  # O/B button to exit
-                speed = 0
-                turn = STRAIGHT
-                print("Exiting program")
-                pygame.quit()
-                sys.exit()
-            elif event.button == 0:  # X/A button
-                is_recording = True
-                print("Record started")
-                folder_path = create_new_folder()
-            elif event.button == 2:  # □/X button
-                is_recording = False
-                print("Record stopped")
-
+def quit():
+    communication.finished = True
 
 def main():
-    print(
-        "Use arrow keys to control the robot. Press P/K to switch between controller and keyboard. Press 'Q' to "
-        "quit.")
-    CONTROLLER_MODE = 1
-    KEYBOARD_MODE = 0
+    step = 0
+    decisions = {
+        "speed": [],
+        "turn": []
+    }
+
+    i = 0
+    folder = Path("images/")
+    while folder.exists():
+        folder = Path(f"images{i}/")
+        i += 1
+
+    folder.mkdir(parents=True)
+
+    k = Keyboard()
+    k.enable()
+
+    print()
+    print("-- ROBO DATA COLLECTOR v1 --")
+    print("Press:")
+    print("a / left arrow -> To turn wheels left")
+    print("d / right arrow -> To turn wheels right")
+    print("w / up arrow -> To go forward")
+    print("s / down arrow -> To go backwards")
+    print("q -> Quit")
+    print()
+    print(f"Images are automatically saved to {folder.absolute().as_posix()}")
+    print()
+    print("NOTE: It is highly recommended to turn on readonly mode in your terminal !")
+    print()
 
     try:
-        pygame.init()
-        pygame.joystick.init()
-        joystick = pygame.joystick.Joystick(0)
-        joystick.init()
-        controller_detected = True
-        mode = CONTROLLER_MODE
-        print("Joystick detected.")
-    except pygame.error:
-        controller_detected = False
-        print("No joystick detected. Using keyboard mode only.")
-        mode = KEYBOARD_MODE
+        while not communication.finished:
+            time.sleep(0.6)  # secons to make a decision
+            if communication.send_command():
+                step += 1
+                communication.download_and_save_photo(folder / f"{step}.png")
+                decisions["speed"].append(communication.prev_speed)
+                decisions["turn"].append(communication.prev_turn)
+            communication.reset()
+    except InterruptedError:
+        print("Got interrupt - quiting ...")
 
-    while True:
-        global speed, turn, is_recording, folder_path
-        if keyboard.is_pressed("p") and controller_detected:
-            mode = CONTROLLER_MODE
-        elif keyboard.is_pressed("k"):
-            mode = KEYBOARD_MODE
+    communication.reset(full=True)
+    communication.send_command()
 
-        if mode == KEYBOARD_MODE:
-            handle_keyboard()
-        else:
-            try:
-                handle_controller()
-            except pygame.error:
-                pass
-
-        if keyboard.is_pressed("q"):
-            speed = 0
-            turn = STRAIGHT
-            print("Exiting program.")
-            pygame.quit()
-            sys.exit()
-
-        if is_recording and speed != 0:
-            download_and_save_photo()
-        time.sleep(0.1)
-        send_command()
-        time.sleep(0.1)
-
+    decisions_path = folder / "decisions.json"
+    with open(decisions_path, "w") as f:
+        json.dump(decisions, f)
+    print(f"Saved decisions into {decisions_path.absolute().as_posix()}")
 
 if __name__ == "__main__":
     main()
